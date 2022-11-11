893ee10acf3f99e0771b55b7896066e64d49d073
skedwards88 <skedwards88@github.com>
2022-11-11 08:47:01 -0800

    add rules demo

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,18 +1,20 @@
 import React from "react";
 import packageJson from "../../package.json";
 
-export default function Rules({setDisplay}) {
-  
+export default function Rules({ setDisplay }) {
   return (
-    <div className="App info">
-      <div className="infoText">
-        {<h1>Palette</h1>}
-        {`Build words that match the color patterns by swiping to connect adjacent letters.\n\nTap on a pattern to get a hint.\n\n`}
-        {<small>version {packageJson.version}</small>}
-      </div>
-      <button className="close" onClick={() => setDisplay("game")}>
+    <div className="App rules">
+      <h1 id="rulesHeader">Palette</h1>
+      <p id="rulesText">{`Swipe to connect letters into words that match the color patterns.\n\nTap on a pattern to get a hint.`}</p>
+      <div id="rulesDemo"></div>
+      <button
+        id="rulesClose"
+        className="close"
+        onClick={() => setDisplay("game")}
+      >
         CLOSE
       </button>
+      <small id="rulesVersion">version {packageJson.version}</small>
     </div>
   );
 }

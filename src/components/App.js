4dd7d345d2a4d68643dee79147d13612599b0bff
skedwards88 <skedwards88@github.com>
2022-11-27 14:56:20 -0800

    prettier

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,27 @@ import Rules from "./Rules";
 
 export default function App() {
   const [display, setDisplay] = React.useState("game");
-  
-  const savedState = JSON.parse(localStorage.getItem("dailyPaletteIsFirstGame"));
-  const [isFirstGame, setIsFirstGame] = React.useState(savedState ?? true)
-  
+
+  const savedState = JSON.parse(
+    localStorage.getItem("dailyPaletteIsFirstGame")
+  );
+  const [isFirstGame, setIsFirstGame] = React.useState(savedState ?? true);
+
   React.useEffect(() => {
-    window.localStorage.setItem("dailyPaletteIsFirstGame", JSON.stringify(isFirstGame));
+    window.localStorage.setItem(
+      "dailyPaletteIsFirstGame",
+      JSON.stringify(isFirstGame)
+    );
   }, [isFirstGame]);
 
   if (isFirstGame) {
-    return <Rules setDisplay={setDisplay} isFirstGame={isFirstGame} setIsFirstGame={setIsFirstGame}></Rules>;
+    return (
+      <Rules
+        setDisplay={setDisplay}
+        isFirstGame={isFirstGame}
+        setIsFirstGame={setIsFirstGame}
+      ></Rules>
+    );
   }
 
   switch (display) {

1d4ddf1b55c794dfc2230e10c148cb87f23d6d1b
skedwards88 <skedwards88@github.com>
2023-01-01 11:17:47 -0800

    change beat to win

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,7 +5,7 @@ export default function Rules({ setDisplay, isFirstGame, setIsFirstGame, setSawW
   return (
     <div className="App rules">
       <h1 id="rulesHeader">How to play</h1>
-      <p id="rulesText">{`Swipe to join connecting letters into words that match the color patterns.\n\nTap on a pattern to get a hint.\n\n The game is easier on Monday and gets harder over the week. Can you beat every day?`}</p>
+      <p id="rulesText">{`Swipe to join connecting letters into words that match the color patterns.\n\nTap on a pattern to get a hint.\n\n The game is easier on Monday and gets harder over the week. Can you win every day?`}</p>
       <div id="rulesDemo"></div>
       <button
         id="rulesClose"

436abd915da5a8fe4c7444cc842972e4669bfad7
skedwards88 <skedwards88@github.com>
2023-10-28 12:56:59 -0700

    include game name

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,7 +4,7 @@ import packageJson from "../../package.json";
 export default function Rules({ setDisplay, isFirstGame, setIsFirstGame, setSawWhatsNew }) {
   return (
     <div className="App rules">
-      <h1 id="rulesHeader">How to play</h1>
+      <h1 id="rulesHeader">Lexlet: How to play</h1>
       <p id="rulesText">{`Swipe to join connecting letters into words that match the color patterns.\n\nTap on a pattern to get a hint.\n\n The game is easier on Monday and gets harder over the week. Can you win every day?`}</p>
       <div id="rulesDemo"></div>
       <button

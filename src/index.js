02aa96ee3ed4ecb02c2c819001635cd2a41a47bd
skedwards88 <skedwards88@gmail.com>
2025-03-07 16:04:35 -0800

    reorg styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import {createRoot} from "react-dom/client";
 import React from "react";
 import App from "./components/App";
-import "./App.css";
-import "./Settings.css";
+import "./styles/App.css";
+import "./styles/ControlBar.css";
+import "./styles/Stats.css";
+import "./styles/Settings.css";
+import "./styles/LargeScreen.css";
 
 if (process.env.NODE_ENV !== "development" && "serviceWorker" in navigator) {
   const path = "/service-worker.js";
